eccc0cdc1f0ec5aa62b71259e9306ac3e2ff40c1
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-07-01 00:58:50 +0530

    Collection ;Number of Count

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CollectionsProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 4; // Total number of the chapters
+            int[,] chapters = { { 1, 4 }, { 5, 7 }, { 9, 16 }, { 17, 26 } }; // Start and end pages of each chapter given in problem
+            int K = 7; // Current reading page
+
+            int remainingChapters = CountRemainingChapters(N, chapters, K);
+            Console.WriteLine("Number of chapters Remaining to read: " + remainingChapters);
+        }
+
+        static int CountRemainingChapters(int N, int[,] chapters, int K)
+        {
+            int remainingChapters = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                int chapterStart = chapters[i, 0];
+                int chapterEnd = chapters[i, 1];
+
+                if (chapterStart <= K && K <= chapterEnd)
+                {
+                    remainingChapters = N - i;
+                    break;
+                }
+            }
+
+            return remainingChapters;
+        }
+    }
+}
